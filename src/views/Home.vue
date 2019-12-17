fd1dbad8db82c2a06fcd55654c6bdc8f4cdf6383
panwl <panwl@authine.com>
2019-12-17 19:33:50 +0800

    背景图片

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    asdasdsaddsadasd
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
+<style lang="less" scoped>
+.imgs {
+  background-size: 100% auto;
+  background-repeat: no-repeat;
+  background-image: url(../assets/imgs/0.jpg);
+}
+</style>
+
